d3f353c7de0e3a57bfffe6e99406c70f23ca5d85
Aditya-raj4 <138981169+Aditya-raj4@users.noreply.github.com>
2026-02-04 20:00:08 +0530

    Fix langchain community imports

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitters import CharacterTextSplitter
+from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.llms import HuggingFacePipeline
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.llms import HuggingFacePipeline
 
 from transformers import pipeline
 
